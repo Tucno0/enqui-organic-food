5f9b0fc9fba547ef6217f0d262b4cf520deb5307
yemerPx <eduardrivera2000@gmail.com>
2023-11-04 10:00:44 -0500

    add pages  login

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { InicioPageComponent } from './pages/inicio-page/inicio-page.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
-    InicioPageComponent
+    InicioPageComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
